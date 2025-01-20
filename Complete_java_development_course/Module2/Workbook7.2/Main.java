bd75ad237662654fa1d9eb13fc98accd47221b24
yr-nitish <nitishgupta1601@gmail.com>
2025-01-20 21:53:43 +0530

    Added code for Constructors

---
+++ Complete_java_development_course/Module2/Workbook7.2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Person person = new Person("Nitish", "Indian", "01/01/2011", 15);
+        System.out.println("Name:  " +person.name + "\n" + "Nationality: " +person.nationality +"\n" + "Date of Birth: "+person.dob+ "\n"+ "Seat number: "+ person.seatNumber );
+    }
+    
+}
---
+++ Complete_java_development_course/Module2/Workbook7.2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    
+    String name;
+    String nationality;
+    String dob;
+    int seatNumber;
+    public String dateOfBirth;
+    public String[] passport;
+
+    public Person(String name, String nationality, String dob, int seatNumber)
+    {
+        this.name = name;
+        this.nationality = nationality;
+        this.dob = dob;
+        this.seatNumber = seatNumber;
+    }
+
+    public Person() {
+        //TODO Auto-generated constructor stub
+    }
+    
+}
