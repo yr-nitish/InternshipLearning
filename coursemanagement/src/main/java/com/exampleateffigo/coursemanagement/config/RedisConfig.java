dce1c93522bfeff44b42130fed2e76ec877732b2
yr-nitish <nitishgupta1601@gmail.com>
2025-04-21 10:11:57 +0530

    few additions

---
+++ coursemanagement/src/main/java/com/exampleateffigo/coursemanagement/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.exampleateffigo.coursemanagement.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${cache.redis.ttl:10}")
+    private String redisTtlStr;
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        long redisTtl = Long.parseLong(redisTtlStr);
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(redisTtl))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .withInitialCacheConfigurations(Map.of("coursemanagement", cacheConfig))
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+}
